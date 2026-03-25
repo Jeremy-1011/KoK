8935b92e5f9879285c532d5e21d85d486cebe816
Jeremy <jerrytimi.ayeni@gmail.com>
2026-03-25 14:52:45 +0000

    Update server.js

--- chat-app/server.js
+++ chat-app/server.js
@@ -38,6 +38,6 @@ socket.on('user joined', (username) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
