08803c4a6ca21701b491122080f9447f31390a09
Jeremy <jerrytimi.ayeni@gmail.com>
2026-03-25 14:45:50 +0000

    Update server.js

--- chat-app/server.js
+++ chat-app/server.js
@@ -37,6 +37,7 @@ socket.on('user joined', (username) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
